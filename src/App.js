06a503247c621228a566debb736075e62b87f6a1
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-05-28 10:05:21 -0300

    Added api call with didMount, refactored for new data route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends React.Component {
   state = {
-    robots: robots,
+    robots: [],
     searchField: ''
   };
+
+  componentDidMount= async () => {
+    const data = await fetch('https://jsonplaceholder.typicode.com/users');
+    const json = await data.json();
+    this.setState({ robots: json });
+  }
 
   onSearchChange = (e) => {
     this.setState({ searchField: e.target.value});
@@ -18,6 +23,9 @@ class App extends React.Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     });
+    if(this.state.robots.length === 0) {
+      return <h1 className='tc'>Loading</h1>
+    }
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
