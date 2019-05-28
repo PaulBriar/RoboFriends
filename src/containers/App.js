4e960669c980a57a5bf5b904b2f530f71e6a1f1d
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-05-28 15:10:33 -0300

    Wired up Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,13 +3,14 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
+import { setSearchField } from '../store/actions';
+import { connect } from 'react-redux';
 
 import './App.css';
 
 class App extends React.Component {
   state = {
     robots: [],
-    searchField: ''
   };
 
   componentDidMount= async () => {
@@ -18,12 +19,9 @@ class App extends React.Component {
     this.setState({ robots: json });
   }
 
-  onSearchChange = (e) => {
-    this.setState({ searchField: e.target.value});
-  }
-
   render() {
-    const {robots, searchField} = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
@@ -32,7 +30,7 @@ class App extends React.Component {
     (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
+        <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           <ErrorBoundry>
             <CardList robots={filteredRobots} />
@@ -43,4 +41,16 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+ };
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
