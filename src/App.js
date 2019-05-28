a6031c9f0f0c702b4ee8edf66db6a2d626c00292
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-05-28 09:44:49 -0300

    Finished styling

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import { robots } from './robots';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    robots: robots,
+    searchField: ''
+  };
+
+  onSearchChange = (e) => {
+    this.setState({ searchField: e.target.value});
+  }
+
   render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    });
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots} />
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
