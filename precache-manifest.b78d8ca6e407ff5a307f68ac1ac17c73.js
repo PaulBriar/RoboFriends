a5091aabfba4298b9b93076a9c4e6ff0336a181f
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-05-28 10:51:06 -0300

    Updates

---
+++ precache-manifest.b78d8ca6e407ff5a307f68ac1ac17c73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c406a8fb96b18cd7944508443800834",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "3c9ee115910581e82e6f",
+    "url": "/RoboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "0ced6404a55fe16a95a2",
+    "url": "/RoboFriends/static/css/main.a6abc5a8.chunk.css"
+  },
+  {
+    "revision": "3c9ee115910581e82e6f",
+    "url": "/RoboFriends/static/js/2.6f6843af.chunk.js"
+  },
+  {
+    "revision": "0ced6404a55fe16a95a2",
+    "url": "/RoboFriends/static/js/main.5293d2d4.chunk.js"
+  },
+  {
+    "revision": "d48d68f29921c5c588da",
+    "url": "/RoboFriends/static/js/runtime~main.19bee101.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
