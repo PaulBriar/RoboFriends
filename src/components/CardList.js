3b06c9015ad88f0dc4da0d06065895121825434b
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-05-28 10:44:41 -0300

    Refactored folder structure, app complete

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Card from './Card';
 
+import './CardList.css';
+
 const CardList = ({robots}) => {
   const cardComponent = robots.map((robot, i) => {
     return <Card key={i} id={robot.id} name={robot.name} email={robot.email} />
